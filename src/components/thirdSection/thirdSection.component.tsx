7c83a2e8cc68d5c43e8c4fcf72c05b571dfa4e06
Auckfmine <mouhamedaminerouatbi@gmail.com>
2023-11-28 21:39:56 +0100

    changes

--- src/components/thirdSection/thirdSection.component.tsx
+++ src/components/thirdSection/thirdSection.component.tsx
@@ -4,7 +4,7 @@ import TextSlider from '../textSlider/text-slider.component'
 function ThirdSection() { 
   return (
     <>
-    <div style={{height:'100vh'}} id='jumbotron2' className=" mb-12 bg-white w-full h-full">
+    <div id='jumbotron2' className=" mb-12 bg-white w-full h-full">
         <TextSlider></TextSlider>
     </div>
     </>
